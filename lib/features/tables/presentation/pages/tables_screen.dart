82077c5b3cde20e18e169f73cb2fb2540b33b05e
Salih Sarang <sarangsalih@gmail.com>
2026-04-15 16:45:16 +0530

    Standardized TablesScreen to match OrdersPage architecture with flat Scaffold and decentralized state

--- lib/features/tables/presentation/pages/tables_screen.dart
+++ lib/features/tables/presentation/pages/tables_screen.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rms_design_system/rms_design_system.dart';
 import 'package:waiter_portal/core/di/injector.dart';
 
 import '../bloc/table_view_bloc.dart';
 import '../bloc/table_view_event.dart';
-import '../bloc/table_view_state.dart';
-import '../widgets/tables_screen/tables_error_view.dart';
-import '../widgets/tables_screen/tables_loading_view.dart';
-import '../widgets/tables_screen/tables_success_view.dart';
+import '../widgets/tables_screen/hall_selector_section.dart';
+import '../widgets/tables_screen/tables_screen_app_bar.dart';
 
 class TablesScreen extends StatelessWidget {
   const TablesScreen({super.key});
@@ -16,21 +15,12 @@ class TablesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => getIt<TableViewBloc>()..add(TableViewInit()),
-      child: BlocBuilder<TableViewBloc, TableViewState>(
-        builder: (context, state) {
-          if (state.isLoading && state.halls.isEmpty) {
-            return const TablesLoadingView();
-          }
-
-          if (state.error != null && state.halls.isEmpty) {
-            return TablesErrorView(
-              error: state.error!,
-              onRetry: () => context.read<TableViewBloc>().add(TableViewInit()),
-            );
-          }
-
-          return const TablesSuccessView();
-        },
+      child: const Scaffold(
+        backgroundColor: NeutralColors.background,
+        appBar: TablesScreenAppBar(),
+        body: SingleChildScrollView(
+          child: Column(children: [HallSelectorSection()]),
+        ),
       ),
     );
   }
