3930eb83f97e0a4319242ce6b95fb82dd5cc535e
Salih Sarang <sarangsalih@gmail.com>
2026-05-20 14:43:01 +0530

    fix: resolve merge artifacts and analyzer issues

--- lib/features/orders/presentation/widgets/food_details/bottom_action_bar.dart
+++ lib/features/orders/presentation/widgets/food_details/bottom_action_bar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rms_design_system/rms_design_system.dart';
+import '../../bloc/food_details/food_details_cubit.dart';
 import 'bottom_action_widgets.dart';
 import 'food_details_actions.dart';
 
@@ -8,14 +10,21 @@ class BottomActionBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final hasSelectedPortion =
+        context.watch<FoodDetailsCubit>().state.selectedPortion != null;
+
     return Container(
       padding: const EdgeInsets.all(20),
       decoration: const BoxDecoration(
         color: NeutralColors.background,
         border: Border(top: BorderSide(color: NeutralColors.border, width: 1)),
       ),
-      child: const Row(
-        children: [QuantitySelector(), SizedBox(width: 16), AddToOrderButton()],
+      child: Row(
+        children: [
+          const QuantitySelector(),
+          const SizedBox(width: 16),
+          AddToOrderButton(hasSelectedPortion: hasSelectedPortion),
+        ],
       ),
     );
   }
